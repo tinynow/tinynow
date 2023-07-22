05144edf48e40a9ef3d5d8f9447532a7c552505f
tinynow <matt@tinynow.com>
2023-07-22 16:27:11 -0700

    see all color options

---
+++ tokens.js
@@ -0,0 +1,43 @@
+/* Import tokens from CSS */
+/* Single source of truth is the CSS */
+// TODO: handle this in the build process
+
+function getTokens() {
+    const isSameDomain = (styleSheet) => {
+        if (!styleSheet.href) {
+            return true;
+        }
+
+        return styleSheet.href.indexOf(window.location.origin) === 0;
+    };
+
+    const isStyleRule = (rule) => rule.type === 1;
+
+    const getCSSCustomProps = () =>
+        [...document.styleSheets].filter(isSameDomain).reduce(
+            (finalArr, sheet) =>
+                finalArr.concat(
+                    [...sheet.cssRules].filter(isStyleRule).reduce((propValArr, rule) => {
+                        const props = [...rule.style]
+                            .map((propName) => [
+                                propName.trim(),
+                                rule.style.getPropertyValue(propName).trim()
+                            ])
+                            .filter(([propName]) => propName.indexOf("--") === 0);
+
+                        return [...propValArr, ...props];
+                    }, [])
+                ),
+            []
+        );
+
+    // return an object with the tokens
+    return getCSSCustomProps().reduce((tokens, [propName, propValue]) => {
+        return {
+            ...tokens,
+            [propName]: propValue
+        };
+    }, {});
+}
+const tokens = getTokens();
+export { tokens };
